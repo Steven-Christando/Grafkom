49f1902a7946ec779bf0af7fa803a1422f16e267
Steven-Christando <97154991+Steven-Christando@users.noreply.github.com>
2022-04-16 18:01:57 +0700

    Add files via upload

---
+++ Karakter.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LearnOpenTK.Common;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+
+namespace UTSgrafkom
+{
+    internal class Karakter : Asset3d
+    {
+        float degr = 0;
+        double time;
+        List<Asset3d> listObject = new List<Asset3d>();
+        Asset3d kepala, bokong, google, tas, badan, kakiKiri, kakiKanan;
+
+        float x, y, z;
+        static class Constants
+        {
+            public const string path = "D:../../../shader/";
+        }
+        public Karakter(float x, float y, float z, Vector3 color) : base(color)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+
+            kepala = new Asset3d(new Vector3(0, 0.5f, 1));
+            kepala.createEllipsoid(x + (-0.5f), y + (-0.3f), z + (0), 0.2f, 0.2f, 0.1f, 12, 12);
+
+            bokong = new Asset3d(new Vector3(0, 0.5f, 1));
+            bokong.createEllipsoid(x + (-0.5f), y + (-0.55f), z + (0), 0.2f, 0.15f, 0.1f, 12, 12);
+
+            google = new Asset3d(new Vector3(0.8f, 0.8f, 0.8f));
+            google.createEllipsoid(x + (-0.412f), y + (-0.3f), z + (0), 0.123f, 0.075f, 0.1f, 12, 12);
+
+            tas = new Asset3d(new Vector3(1f, 0.5f, 1f));
+            tas.createBoxVertices(x+(-0.555f),y+(-0.4f),z+(-0.05f));
+
+            badan = new Asset3d(new Vector3(0, 0.5f, 1));
+            badan.tabung(x+(-0.5f), y+(0), z + (0.55f), 0.2f, 0.1f, 0.013f);
+
+            kakiKiri = new Asset3d(new Vector3(0, 0.5f, 1f));
+            kakiKiri.tabung(x+(-0.62f), y+(0), z + (0.8f), 0.075f, 0.05f, 0.017f);
+
+            kakiKanan = new Asset3d(new Vector3(0, 0.5f, 1f));
+            kakiKanan.tabung(x + (-0.3753f), y + (0), y + (0.8f), 0.075f, 0.05f, 0.017f);
+
+            listObject.Add(tas);
+            listObject.Add(kakiKanan);
+            listObject.Add(kakiKiri);
+            listObject.Add(kepala);
+            listObject.Add(bokong);
+            listObject.Add(badan);
+            listObject.Add(google);
+        }
+
+        public void load(float SizeX,float SizeY)
+        {
+            foreach (Asset3d i in listObject)
+            {
+                i.load(Constants.path + "shader.vert", Constants.path + "shader.frag", SizeX, SizeY);
+            }
+        }
+
+        public void render(double times, Matrix4 temps, Matrix4 cameraView, Matrix4 cameraProjection)
+        {
+            time += 15.0 * times;
+            Matrix4 temp = Matrix4.Identity;
+            for (int i = 0; i < listObject.Count; i++)
+            {
+                if (i == 1 || i == 2 || i == 5)
+                {
+                    degr = MathHelper.DegreesToRadians(90f);
+                    temp = Matrix4.CreateRotationX(degr)*temps;
+                }
+                else
+                {
+                    degr = MathHelper.DegreesToRadians(00f);
+                    temp = Matrix4.CreateRotationX(degr)*temps;
+                }
+                listObject[i].render(1, temp, time, cameraView, cameraProjection);
+            }
+        }
+
+    }
+}
---
+++ Windows.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LearnOpenTK.Common;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+namespace UTSgrafkom
+{
+    static class Constants
+    {
+        public const string path = "D:../../../shader/";
+    }
+    internal class Windows : GameWindow
+    {
+        float degr = 0;
+        double time;
+        List<Asset3d> listObject = new List<Asset3d>();
+        float x = 0;
+        Karakter karakter;
+        ruangan room;
+        Camera camera;
+
+        Matrix4 temp2 = Matrix4.Identity;
+
+        public Windows(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) : base(gameWindowSettings, nativeWindowSettings)
+        {
+            
+        }
+
+        protected override void OnLoad()
+        {
+
+            base.OnLoad();
+            GL.Enable(EnableCap.DepthTest);
+            GL.ClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+            room = new ruangan(new Vector3(0.6f, 0.5f, 0.5f));
+            room.createBoxVertices(0, 0, 0);
+            karakter = new Karakter(0f, 0f, 0f, new Vector3(0, 0.5f, 1));
+            karakter.load(Size.X,Size.Y);
+            /*room.load(Constants.path + "shader.vert", Constants.path + "shader.frag", Size.X, Size.Y);*/
+            camera = new Camera(new Vector3(0, 0, 1), Size.X / (float)Size.Y);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            base.OnRenderFrame(args);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            time += 15.0 * time;
+            Matrix4 temp = Matrix4.Identity;
+            degr = MathHelper.DegreesToRadians(0.5f);
+            temp = Matrix4.CreateRotationY(degr);
+
+            x += 0.001f;
+            temp2 *= Matrix4.CreateTranslation(new Vector3(0.0f, 0f,0f));
+
+            /*room.render(1, temp, time, camera.GetViewMatrix(), camera.GetProjectionMatrix());*/
+            karakter.render(args.Time,temp * temp2, camera.GetViewMatrix(), camera.GetProjectionMatrix());
+            SwapBuffers();
+        }
+
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            base.OnResize(e);
+            /*Console.WriteLine("halo");*/
+            GL.Viewport(0, 0, Size.X, Size.Y);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+            var input = KeyboardState;
+            float cameraSpeed = 0.5f;
+            if (input.IsKeyDown(Keys.Escape))
+            {
+                Close();
+            }
+            if (input.IsKeyDown(Keys.W))
+            {
+                camera.Position += camera.Front * cameraSpeed * (float)args.Time;
+            }
+            if (input.IsKeyDown(Keys.A))
+            {
+                camera.Position -= camera.Right * cameraSpeed * (float)args.Time;
+                /*Console.WriteLine("tombol a di release");*/
+            }
+            if (input.IsKeyDown(Keys.S))
+            {
+                camera.Position -= camera.Front * cameraSpeed * (float)args.Time;
+            }
+            if (input.IsKeyDown(Keys.D))
+            {
+                camera.Position += camera.Right * cameraSpeed * (float)args.Time;
+            }
+
+            /*if (input.IsKeyDown(Keys.Up))
+            {
+                camera.Position += camera.Pitch * cameraSpeed * (float)args.Time;
+            }
+            if (input.IsKeyDown(Keys.Down))
+            {
+                camera.Position -= camera.Pitch * cameraSpeed * (float)args.Time;
+            }*/
+        }
+    }
+}
